0ae0ca3f333d51fe0342b62edc028b002bd6eafc
nourhesham116 <nourhesham116@gmail.com>
2023-05-13 01:38:17 +0300

    server is working and linked to index only via ejs

---
+++ server.js
@@ -0,0 +1,31 @@
+//imports
+const express=require('express')
+const app=express()
+const port=3000
+//static files
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname +'public/css'))
+app.use('/js',express.static(__dirname +'public/javascript'))
+app.use('/imgs',express.static(__dirname +'public/imgs'))
+//set views
+app.set('views','./views')
+app.set('view engine','ejs')
+
+app.get('',(req,res)=>{
+    res.render('ayhaga')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//listening on port
+app.listen(port,()=> console.info(`listening on port ${port}`))

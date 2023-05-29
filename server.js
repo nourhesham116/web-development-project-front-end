21568f34efa6f0b8bd830572ba6ea88d64d7f0d6
FatimaAl-najjar <125657503+FatimaAl-najjar@users.noreply.github.com>
2023-05-29 10:59:29 +0300

    fixed bug

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) => {
   res.render('index', { user: (req.session.user === undefined ? "" : req.session.user) })
 })
 
-app.use('/body', prodRouter);
+app.use('/', prodRouter);
 app.use('/admindashboard',admindashboardRouter)
 app.use('/addproduct-action',addproductsRouter)
 app.get('', (req, res) => {
@@ -215,7 +215,7 @@ app.get('/allface', (req, res) => {
   res.render('allface', { user: (req.session.user === undefined ? "" : req.session.user) })
 })
 app.get('/Liptreatment', (req, res) => {
-  res.render('Lipbalm', { user: (req.session.user === undefined ? "" : req.session.user) })
+  res.render('Liptreatment', { user: (req.session.user === undefined ? "" : req.session.user) })
 })
 app.get('/skincarePage1', (req, res) => {
   res.render('skincarePage1', { user: (req.session.user === undefined ? "" : req.session.user) })

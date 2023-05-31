a63b6d24d7f49319e1341d33e4666cf3d0257911
FatimaAl-najjar <125657503+FatimaAl-najjar@users.noreply.github.com>
2023-05-31 17:24:23 +0300

    git bash mknsh 3amel  push kol el updates

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -57,4 +57,78 @@ router.get('/newmeltawf', function (req, res, next) {
           })
   });
 });
+
+router.get('/new', function (req, res, next) {
+  Product.find({ type: 'new' }).then(function (product) {
+    res.render('Skinproducts', {
+            productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+          })
+  });
+});
+
+router.get('/bodymists', function (req, res, next) {
+  Product.find({ type: 'body mists' }).then(function (product) {
+    res.render('Skinproducts', {
+            productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+          })
+  });
+});
+
+
+router.get('/newplushpuddin', function (req, res, next) {
+  Product.find({ name: 'Fenty Plush Puddin lip mask' }).then(function (product) {
+    res.render('Skinproducts', {
+            productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+          })
+  });
+});
+
+router.get('/allskin', function (req, res, next) {
+  Product.find({ type:"SKIN" }).then(function (product) {
+    res.render('Skinproducts', {
+            productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+          })
+  });
+});
+router.get('/cleanser', function (req, res, next) {
+  Product.find({ type: 'cleanser' }).then(function (product) {
+    res.render('Skinproducts', {
+            productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+          })
+  });
+});
+
+router.get('/toner', function (req, res, next) {
+  Product.find({ type: 'toner' }).then(function (product) {
+    res.render('Skinproducts', {
+            productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+          })
+  });
+});
+
+router.get('/lipcare', function (req, res, next) {
+  Product.find({ type: 'lip care' }).then(function (product) {
+    res.render('Skinproducts', {
+            productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+          })
+  });
+});
+
+router.get('/masks', function (req, res, next) {
+  Product.find({ type: 'mask and treatment' }).then(function (product) {
+    res.render('Skinproducts', {
+            productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+          })
+  });
+});
+
+
+router.get('/eyecream', function (req, res, next) {
+  Product.find({ type: 'eye cream' }).then(function (product) {
+    res.render('Skinproducts', {
+            productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+          })
+  });
+});
+
 module.exports = router;

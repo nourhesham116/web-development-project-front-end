bc15805715bfd4292951aeba45d701eb744108e4
FatimaAl-najjar <125657503+FatimaAl-najjar@users.noreply.github.com>
2023-05-31 04:03:51 +0300

    https://github.com/nourhesham116/web-development-project-front-end

--- routes/bproductsRoute.js
+++ routes/bproductsRoute.js
@@ -5,16 +5,16 @@ const productController = require('../controllers/product.controller');
 
 
 
-  router.get('/allface', function(req, res, next) {
-    Product.find({type:'Face moisturizer'}).then(function(products){
+  // router.get('/allface', function(req, res, next) {
+  //   Product.find({type:'Face moisturizer'}).then(function(products){
         
-        res.render('allface',
-              {
-                productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
-              })
-          })
+  //       res.render('allface',
+  //             {
+  //               productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+  //             })
+  //         })
 
-    });
+  //   });
     
   
 

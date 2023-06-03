71801233eb1429d88cf6cfe8b9425e28a10456ed
FatimaAl-najjar <125657503+FatimaAl-najjar@users.noreply.github.com>
2023-06-03 11:44:03 +0300

    injecting data fe beauty side

--- public/javascript/productcontrol.js
+++ public/javascript/productcontrol.js
@@ -34,7 +34,7 @@ document.getElementById("newproductdetails").addEventListener("submit", function
 
 });
 const arrSkin=["new","Face moisturizer","face cleanser","toner","mask and treatment","lip care","eye cream","suncream","body cleanser","scrub","body mists","body moisturizer"];
-const arrBeauty=["foundation","prime+set","concelar","powder","contour","blush","bronzing","highlighter","brushes","lipgloss","lipstick","lipcombos","lipbalm","eyeshadow","mascara"]
+const arrBeauty=["foundation","prime+set","concelar","powder","blush","bronzing","highlighter","brushes","lipgloss","lipstick","lipcombos","lipbalm","eyeshadow","mascara","eyeliner"];
 function dropdownSkin(){
     let drop=document.getElementById("custom-select");
     var option;
--- routes/bproductsRoute.js
+++ routes/bproductsRoute.js
@@ -30,6 +30,88 @@ router.get('/', function (req, res, next) {
     })
   
   });
-  
 
+  router.get('/Lipgloss', function (req, res, next) {
+    Product.find({ type: 'lipgloss' }).then(function (product) {
+      res.render('Beautyproducts', {
+        productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+      })
+    })
+  
+  });
+  
+  router.get('/Highlighters', function (req, res, next) {
+    Product.find({ type: 'highlighter' }).then(function (product) {
+      res.render('Beautyproducts', {
+        productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+      })
+    })
+  
+  });
+
+  router.get('/Blush', function (req, res, next) {
+    Product.find({ type: 'blush' }).then(function (product) {
+      res.render('Beautyproducts', {
+        productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+      })
+    })
+  
+  });
+
+  router.get('/Brushtools', function (req, res, next) {
+    Product.find({ type: 'brushes' }).then(function (product) {
+      res.render('Beautyproducts', {
+        productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+      })
+    })
+  
+  });
+
+  router.get('/Eyeshadows', function (req, res, next) {
+    Product.find({ type: 'eyeshadow' }).then(function (product) {
+      res.render('Beautyproducts', {
+        productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+      })
+    })
+  });
+
+  router.get('/eyeliner', function (req, res, next) {
+    Product.find({ type:'eyeliner'}).then(function (product) {
+      res.render('Beautyproducts', {
+        productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+      })
+    })
+  });
+
+  router.get('/concealer', function (req, res, next) {
+    Product.find({ type:'concelar'}).then(function (product) {
+      res.render('Beautyproducts', {
+        productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+      })
+    })
+  });
+
+  router.get('/foundation', function (req, res, next) {
+    Product.find({ type:'foundation'}).then(function (product) {
+      res.render('Beautyproducts', {
+        productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+      })
+    })
+  });
+
+  router.get('/powder', function (req, res, next) {
+    Product.find({ type:'powder'}).then(function (product) {
+      res.render('Beautyproducts', {
+        productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+      })
+    })
+  });
+  router.get('/prime', function (req, res, next) {
+    Product.find({ type:'prime+set'}).then(function (product) {
+      res.render('Beautyproducts', {
+        productsList: product, userP: req.session.user, user: (req.session.user === undefined ? "" : req.session.user)
+      })
+    })
+  });
+  
   module.exports = router;
